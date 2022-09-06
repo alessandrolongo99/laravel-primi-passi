9cf1d16320dc1c93336fd60e17c3e5177591e8ee
alessandrolongo99 <longoalessandro153@gmail.com>
2022-09-06 15:38:29 +0200

    hello laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<style>
+    h1{
+        color: red;
+        text-align: center;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+</style>
+
+<body>
+    <h1>HELLO LARAVEL!</h1>
+</body>
+</html>
